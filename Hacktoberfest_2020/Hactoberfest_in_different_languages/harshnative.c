3648749bc69b5c458d814a2912d8842b26434676
Madhuban Dubey <72182409+madhubandubey9@users.noreply.github.com>
2021-10-03 01:31:36 -0400

    Update harshnative.c

--- Hacktoberfest_2020/Hactoberfest_in_different_languages/harshnative.c
+++ Hacktoberfest_2020/Hactoberfest_in_different_languages/harshnative.c
@@ -3,5 +3,5 @@ int main() {
    // printf() displays the string inside quotation
    printf("हैक्टोबर त्योहार");
    printf("If you have some interest in technology, try this fest and enjoy!");
-   return 0;
+   return (0);
 }
